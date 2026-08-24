7da4f3164d6d73782fe864b44fb1cc392aa860b2
sidewinder00 <33611297+sidewinder00@users.noreply.github.com>
2026-08-23 20:00:05 -0500

    Publish v0.1.5 constants

---
+++ custom_components/tcx_direct/const.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+DOMAIN = "tcx_direct"
+NAME = "Jandy TCX Direct"
+VERSION = "0.1.5"
+
+API_KEY = "EOOEMOW4YR6QNB07"
+ZODIAC_API = "https://prod.zodiac-io.com"
+IAQUALINK_API = "https://r-api.iaqualink.net"
+WEBSOCKET_URL = "wss://prod-socket.zodiac-io.com/devices"
+
+CONF_DEVICE_ID = "device_id"
+CONF_DEVICE_NAME = "device_name"
+CONF_DEVICE_TYPE = "device_type"
+
+PLATFORMS = ["sensor", "binary_sensor", "button"]
+
+SHADOW_INTERVAL = 60
+TOKEN_REFRESH_MARGIN = 300
+RECONNECT_MAX = 60
+MAX_WEBSOCKET_SESSION = 900
+WEBSOCKET_STALE_SECONDS = 600
+RECENT_WS_STRUCTURES = 20
+BOOTSTRAP_SUBSCRIBE_INTERVAL = 3
+BOOTSTRAP_SUBSCRIBE_ATTEMPTS = 4
+BOOTSTRAP_WAIT_SECONDS = 12
+CACHE_VERSION = 1
